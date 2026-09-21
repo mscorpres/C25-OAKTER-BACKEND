ef257af7db29808b7ebd071e033bef9fa01cfea7
Aman-mscorpres <aman.kumar@mscorpres.in>
2026-09-21 13:25:19 +0530

    print auth fixes

--- helper/PRINT/PHP/authConfig/alwarBackendConfig.php
+++ helper/PRINT/PHP/authConfig/alwarBackendConfig.php
@@ -2,13 +2,14 @@
     date_default_timezone_set('Asia/Kolkata');
     
     define('DB_OAKTER_HOST',"138.201.35.175"); 
-    define('DB_OAKTER_USER',"msc_ims_user");
-    define('DB_OAKTER_PASS',"a9vLc4/&@DgKN1>A");
+    define('DB_OAKTER_USER',"usr_oakter_ims");
+    define('DB_OAKTER_PASS',"%89s1r5Uqp%3r6uD37");
     
-    define('DB_OAKTER_INVT', 'alwar_oakter_ims_invt');
-    define('DB_OAKTER_TALLY', 'alwar_oakter_ims_tally');
+    define('DB_OAKTER_INVT', 'c25_ims_invt');
+    define('DB_OAKTER_TALLY', 'c25_ims_finance');
     
     // Establish DB Connection
+    
     
     try {
         $con = new PDO("mysql:host=" . DB_OAKTER_HOST . ";charset=UTF8;dbname=" . DB_OAKTER_INVT, DB_OAKTER_USER, DB_OAKTER_PASS);
